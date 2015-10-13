63d23c5993946cd0987530d6a08f325ab6cd6e07
RJustice <ivanyun.t@gmail.com>
2015-10-13 17:34:47 +0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,7 +19,6 @@ class HomeController extends Controller
      */
     public function index()
     {   
-        \App\DealOrder::buildReturns();dd(1);
         $official_news = Category::where('alias','official-news')->first()->articles()->orderBy('id','desc')->take(4)->get();
         $industry_news = Category::where('alias','industry-news')->first()->articles()->orderBy('id','desc')->take(4)->get();
         //$industry_news = Category::where('alias','industry-news')->get();
